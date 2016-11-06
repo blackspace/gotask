05f994f1db8adbab1ab4b7a3d9ddb840ec657132
weiwufeng <weiwufeng@sohu.com>
2016-11-06 17:08:06 +0800

    ...

---
+++ examples/callback.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/blackspace/goevent"
+	"github.com/blackspace/gotask/runnable_pool/event"
+	"github.com/blackspace/gotask/examples/tasks"
+	"log"
+)
+
+var runnable_pool=event.NewRunnablePoolEvent()
+
+func main() {
+	runnable_pool.TaskDoneEvent.AddHandler(func(s goevent.Source,a goevent.EventArg){
+		switch s.(type) {
+		case *tasks.HelloWorld:
+			log.Println(a.(string))
+		}
+
+	})
+
+	go func() {
+		for {
+			runnable_pool.AddTask(tasks.NewHelloWorld())
+		}
+	}()
+
+	runnable_pool.Run()
+	goevent.Run()
+}
